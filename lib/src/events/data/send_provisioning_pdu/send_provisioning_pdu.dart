77d8d8d9689ecbf3e58b30251f17804804321922
Kleak <me@kleak.dev>
2020-08-11 12:29:16 +0200

    Normalize return of unprovisioned mesh node Fixes #29

--- lib/src/events/data/send_provisioning_pdu/send_provisioning_pdu.dart
+++ lib/src/events/data/send_provisioning_pdu/send_provisioning_pdu.dart
@@ -1,11 +1,14 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:nordic_nrf_mesh/src/unprovisioned_mesh_node.dart';
 
 part 'send_provisioning_pdu.freezed.dart';
 part 'send_provisioning_pdu.g.dart';
 
 @freezed
 abstract class SendProvisioningPduData with _$SendProvisioningPduData {
-  const factory SendProvisioningPduData(List<int> pdu, String meshNodeUuid) = _SendProvisioningPduData;
+  @JsonSerializable(explicitToJson: true, anyMap: true)
+  const factory SendProvisioningPduData(List<int> pdu, UnprovisionedMeshNode meshNode) = _SendProvisioningPduData;
 
-  factory SendProvisioningPduData.fromJson(Map<String, dynamic> json) => _$SendProvisioningPduDataFromJson(json);
+  factory SendProvisioningPduData.fromJson(Map<String, dynamic> json) =>
+      _$SendProvisioningPduDataFromJson(json.cast<String, dynamic>());
 }
