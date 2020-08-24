00bda466dfac55c4211e56857855eb4497a6cc02
Kleak <me@kleak.dev>
2020-08-24 11:11:57 +0200

    remove useless code

--- android/src/main/kotlin/fr/dooz/nordic_nrf_mesh/DoozProvisionedMeshNode.kt
+++ android/src/main/kotlin/fr/dooz/nordic_nrf_mesh/DoozProvisionedMeshNode.kt
@@ -47,8 +47,6 @@ class DoozProvisionedMeshNode(binaryMessenger: BinaryMessenger, var meshNode: Pr
             }
             "unicastAddress" -> {
                 result.success(meshNode.unicastAddress)
-                var model = meshNode.elements[0]!!.meshModels[0]!!
-                //model.k
             }
             else -> {
                 result.notImplemented()
