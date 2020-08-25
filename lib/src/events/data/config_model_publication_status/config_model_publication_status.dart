b35218d7b0827488020c18c45c0b23302d0eeed5
Kleak <me@kleak.dev>
2020-08-25 15:58:40 +0200

    Expose ConfigModelPublicationSet Fixes #42

---
+++ lib/src/events/data/config_model_publication_status/config_model_publication_status.dart
@@ -0,0 +1,23 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'config_model_publication_status.freezed.dart';
+part 'config_model_publication_status.g.dart';
+
+@freezed
+abstract class ConfigModelPublicationStatus with _$ConfigModelPublicationStatus {
+  const factory ConfigModelPublicationStatus(
+    int elementAddress,
+    int publishAddress,
+    int appKeyIndex,
+    bool credentialFlag,
+    int publishTtl,
+    int publicationSteps,
+    int publicationResolution,
+    int publishRetransmitCount,
+    int publishRetransmitIntervalSteps,
+    int modelIdentifier,
+  ) = _ConfigModelPublicationStatus;
+
+  factory ConfigModelPublicationStatus.fromJson(Map<String, dynamic> json) =>
+      _$ConfigModelPublicationStatusFromJson(json);
+}
---
+++ lib/src/events/data/config_model_publication_status/config_model_publication_status.g.dart
@@ -0,0 +1,38 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'config_model_publication_status.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_ConfigModelPublicationStatus _$_$_ConfigModelPublicationStatusFromJson(
+    Map<String, dynamic> json) {
+  return _$_ConfigModelPublicationStatus(
+    json['elementAddress'] as int,
+    json['publishAddress'] as int,
+    json['appKeyIndex'] as int,
+    json['credentialFlag'] as bool,
+    json['publishTtl'] as int,
+    json['publicationSteps'] as int,
+    json['publicationResolution'] as int,
+    json['publishRetransmitCount'] as int,
+    json['publishRetransmitIntervalSteps'] as int,
+    json['modelIdentifier'] as int,
+  );
+}
+
+Map<String, dynamic> _$_$_ConfigModelPublicationStatusToJson(
+        _$_ConfigModelPublicationStatus instance) =>
+    <String, dynamic>{
+      'elementAddress': instance.elementAddress,
+      'publishAddress': instance.publishAddress,
+      'appKeyIndex': instance.appKeyIndex,
+      'credentialFlag': instance.credentialFlag,
+      'publishTtl': instance.publishTtl,
+      'publicationSteps': instance.publicationSteps,
+      'publicationResolution': instance.publicationResolution,
+      'publishRetransmitCount': instance.publishRetransmitCount,
+      'publishRetransmitIntervalSteps': instance.publishRetransmitIntervalSteps,
+      'modelIdentifier': instance.modelIdentifier,
+    };
