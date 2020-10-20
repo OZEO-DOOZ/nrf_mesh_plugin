4b640121804eed2ae27339aa47e6203ff2b90387
Kleak <me@kleak.dev>
2020-10-20 13:57:52 +0200

    cleanup

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,32 +1,24 @@
-import 'dart:async';
 import 'dart:isolate';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart' show kDebugMode;
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
-import 'package:pedantic/pedantic.dart';
 
 import 'package:nordic_nrf_mesh_example/src/app.dart';
 
 void main() async {
   if (kDebugMode) {
     await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(false);
-  } else {
-    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
-
-    Isolate.current.addErrorListener(RawReceivePort((pair) async {
-      final List<dynamic> errorAndStacktrace = pair;
-      await FirebaseCrashlytics.instance.recordError(
-        errorAndStacktrace.first,
-        errorAndStacktrace.last,
-      );
-    }).sendPort);
   }
 
-  unawaited(runZonedGuarded<Future<void>>(
-    () async {
-      runApp(MyApp());
-    },
-    FirebaseCrashlytics.instance.recordError,
-  ));
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+
+  Isolate.current.addErrorListener(RawReceivePort((pair) async {
+    final List<dynamic> errorAndStacktrace = pair;
+    await FirebaseCrashlytics.instance.recordError(
+      errorAndStacktrace.first,
+      errorAndStacktrace.last,
+    );
+  }).sendPort);
+  runApp(MyApp());
 }
