ad4fa1e3559ffa90239a306cce9e99f0d0c00cf8
R0m4in-dooz <romain@dooz-domotique.com>
2021-07-12 17:31:58 +0200

    fix import statement

--- lib/src/utils/provisioning.dart
+++ lib/src/utils/provisioning.dart
@@ -1,8 +1,7 @@
 import 'dart:async';
 import 'dart:io';
 
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/services.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
 import 'package:nordic_nrf_mesh/src/ble/ble_mesh_manager.dart';
 import 'package:nordic_nrf_mesh/src/ble/ble_mesh_manager_callbacks.dart';
