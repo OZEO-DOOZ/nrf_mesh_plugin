acb39922ae60b6c3922cbdc217da1b0c11bc0620
R0m4in-dooz <romain@dooz-domotique.com>
2021-10-05 10:24:16 +0200

    allow to only have onError callback

--- lib/src/ble/ble_manager.dart
+++ lib/src/ble/ble_manager.dart
@@ -275,7 +275,7 @@ abstract class BleManager<E extends BleManagerCallbacks> {
             // error may have been caught upon connection initialization or during connection
             GenericFailure? maybeError = connectionStateUpdate.failure;
             // determine if callbacks are set
-            if (_hasDeviceDisconnectedCallback) {
+            if (_hasDeviceDisconnectedCallback || _hasErrorCallback) {
               if (_hasErrorCallback) {
                 // if all callbacks are available, prioritize error callback if any error is given in the event
                 if (maybeError != null) {
